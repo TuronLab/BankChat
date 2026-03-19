5f294731b16f1b0cec68bd173f7f547035af6efb
pablo <pabloturon@hotmail.com>
2026-03-19 23:06:33 +0100

    Added testing to session manage

---
+++ tests/session_manager/test_mockup_repository.py
@@ -0,0 +1,47 @@
+import pytest
+
+from session_manager.models import Client
+from session_manager.session import Session
+from session_manager.session_repository import NoStorageRepository
+
+
+def test_save_and_get_session():
+    repo = NoStorageRepository()
+    dummy_client = Client(client_name="test_client")
+
+    session = Session(client=dummy_client)
+
+    repo.save(session)
+
+    assert repo.get(session.session_id) == session
+
+
+def test_delete_session():
+    repo = NoStorageRepository()
+    dummy_client = Client(client_name="test_client")
+    session = Session(client=dummy_client)
+
+    repo.save(session)
+    repo.delete(session.session_id)
+
+    with pytest.raises(KeyError):
+        repo.get(session.session_id)
+
+
+def test_get_all_sessions():
+    repo = NoStorageRepository()
+
+    dummy_client1 = Client(client_name="test_client1")
+    dummy_client2 = Client(client_name="test_client2")
+    session1 = Session(client=dummy_client1)
+    session2 = Session(client=dummy_client2)
+
+    repo.save(session1)
+    repo.save(session2)
+
+    all_sessions = repo.get_all()
+
+    assert len(all_sessions) == 2
+    assert session1.session_id in all_sessions
+    assert session2.session_id in all_sessions
+    
---
+++ tests/session_manager/test_session_manager.py
@@ -0,0 +1,38 @@
+import pytest
+
+from session_manager.models import Client
+from session_manager.session_manager import SessionManager
+from session_manager.session_repository import NoStorageRepository
+
+
+def test_create_session():
+    repo = NoStorageRepository()
+    manager = SessionManager(repo)
+    dummy_client = Client(client_name="test_client")
+
+    session = manager.create_session(client=dummy_client)
+
+    assert session in repo.get_all().values()
+
+
+def test_get_session():
+    repo = NoStorageRepository()
+    manager = SessionManager(repo)
+    dummy_client = Client(client_name="test_client")
+
+    session = manager.create_session(client=dummy_client)
+    retrieved = manager.get_session(session.session_id)
+
+    assert retrieved == session
+
+
+def test_delete_session():
+    repo = NoStorageRepository()
+    manager = SessionManager(repo)
+    dummy_client = Client(client_name="test_client")
+
+    session = manager.create_session(client=dummy_client)
+    manager.delete_session(session.session_id)
+
+    with pytest.raises(KeyError):
+        manager.get_session(session.session_id)
