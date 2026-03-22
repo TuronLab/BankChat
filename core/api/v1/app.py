5fb114b634a0252ded8fcd9e3014567458dcd77b
pablo <pabloturon@hotmail.com>
2026-03-22 19:30:58 +0100

    Fixed import issues

--- core/api/v1/app.py
+++ core/api/v1/app.py
@@ -4,7 +4,7 @@ from config import CHAT_LOGGER
 
 
 def create_bank_chat_app():
-    from api.v1.router import router
+    from core.api.v1.router import router
     CHAT_LOGGER.info('STARTING BANK CHAT SERVICE')
     app = FastAPI()
     app.include_router(router, tags=['Bank Chat'])
